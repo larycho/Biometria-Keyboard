4fa0a32382ab2fe4624524abe4ceda65c583d8c7
larycho <sample@users.noreply.com>
2025-01-28 17:10:05 +0100

    Measuring time between key press and key release

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from pynput import keyboard
+import time
 
 last_key = -1
+start = time.time()
+end = time.time()
 
 def on_press(key):
-    global last_key
+    global last_key, start
     if last_key == -1:
         try:
             print('alphanumeric key {0} pressed'.format(
                 key.char))
             last_key = key.char
+            start = time.time()
         except AttributeError:
             print('special key {0} pressed'.format(
                 key))
 
 def on_release(key):
-    global last_key
+    global last_key, end, start
     print('{0} released'.format(
         key))
     last_key = -1
+    end = time.time()
+    print('time elapsed: {0} seconds'.format(end - start))
+
     if key == keyboard.Key.esc:
         # Stop listener
         return False
